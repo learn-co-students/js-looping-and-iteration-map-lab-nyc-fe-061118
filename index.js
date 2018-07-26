9e0d20ef2d136f1c556b44b6bdec5db11e407c69
fiolam <fiola.mulliqi@wework.com>
2018-07-26 00:29:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file.
+function lowerCaseDriver(name){
+  return name.toLowerCase()
+}
+
+function lowerCaseDrivers(drivers){
+return drivers.map(lowerCaseDriver)
+}
+
+function createNewObject(name){
+let nameArray = name.split (" ")
+let firstName = nameArray [0]
+let lastName = nameArray [1]
+return {firstName: firstName , lastName: lastName};
+}
+
+function nameToAttributes (drivers) {
+return drivers.map(createNewObject)
+}
+
+function  createNameHometownPhrases(newObject){
+  let nameOnly =  newObject[ 'name']
+  let hometownOnly = newObject ['hometown']
+  return nameOnly + ' is from ' + hometownOnly;
+}
+function attributesToPhrase (driver){
+  return driver.map(createNameHometownPhrases)
+}
